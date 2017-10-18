14b88df8b6bd6012caf7b6434c050d18b38725f7
Heather <hlmathies@gmail.com>
2017-10-18 07:51:46 -0400

    added word constructor function

--- word.js
+++ word.js
@@ -1,2 +1,31 @@
-const fse = require('fs-extra');
-const hangmanUser = require("./letter");
+const Letter = require("./letter");
+
+var word = function(_word){
+  this.letters = [];
+
+  for(i = 0; i < _word.length; i++){
+    var letter = new Letter(_word.charAt(i));
+    this.letters.push(letter);
+  }
+
+  this.checkGuess = function(guess){
+    var guessCorrect = false;
+    for(i in this.letters){
+      if(this.letters[i].checkGuess(guess)){
+        guessCorrect = true;
+      }
+    }
+    return guessCorrect;
+  }
+
+  this.renderWord = function(){
+    var _word = '';
+    for(i in this.letters){
+      _word += this.letters[i].renderLetter() + ' ';
+    }
+    return _word;
+  }
+
+}
+
+module.exports = word;
