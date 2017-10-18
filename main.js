9adeb5b485bffec5e8043dbc708d09d5e3494d2f
Heather <hlmathies@gmail.com>
2017-10-18 07:52:13 -0400

    added console logs to check functionality

--- main.js
+++ main.js
@@ -1,3 +1,41 @@
-// requiring the hangmanUser and hangmanAdmin modules
-const hangmanUser = require("./letter");
-const hangmanAdmin = require("./word");
+// requiring the letter and word modules
+const Word = require("./word");
+
+var _word = new Word('javascript');
+console.log(_word.renderWord());
+_word.checkGuess('a');
+console.log(_word.renderWord());
+
+
+
+// requiring the inquirer or prompt npm package to ask users questions
+// const inquirer = require("inquirer");
+// var count = 0;
+// var letterArray = [];
+//
+// var askForLetter = function(){
+//
+//   if (count < (lengthOfWord)){
+//
+//
+//     inquirer.prompt([
+//     {
+//         name: 'letter',
+//         message: 'Guess a letter?',
+//
+//       }
+//     ]).then(function(answers){
+//       var newLetter = new Letter(
+//         answers.letter);
+//     letterArray.push(newLetter);
+//     count++;
+//     askForLetter();
+//   });
+//
+// }else{
+//   for(){
+//
+//   }
+// }
+// };
+// askForLetter();
